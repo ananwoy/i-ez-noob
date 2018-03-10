e3c0272e1abaac0c3ac0e53c1f1fe4e30996d07d
IDEA-PC\Ananwoy <ananwoy007@gmail.com>
2018-03-11 03:13:04 +0530

    change to mysql

--- asset.sql
+++ asset.sql
@@ -3,16 +3,16 @@
 create database caps_asset;
 
 //Creating the User_Master table and inserting data.
-create table user_master(userid number(5),username varchar2(20),userpassword varchar2(20),usertype varchar2(20));
-insert into user_master values(1,'A','123','admin');
-insert into user_master values(2,'b','456','manager');
-insert into user_master values(3,'c','789','manager');
-insert into user_master values(4,'d','101','manager');
-insert into user_master values(5,'e','102','manager');
-insert into user_master values(6,'f','103','manager');
+create table user_master(userid integer(5),username varchar(20),userpassword varchar(20),usertype varchar(20));
+insert into user_master values(1,'ananwoy','123','admin');
+insert into user_master values(2,'prince','456','manager');
+insert into user_master values(3,'tejashree','456','manager');
+insert into user_master values(4,'shiyon','456','manager');
+insert into user_master values(5,'ashna','456','manager');
+insert into user_master values(6,'abhishek','456','manager');
 
 
-create table asset(assetid number(6) primary key,assetname varchar2(20),assetdes varchar2(50),quantity varchar2(20),status varchar2(20));
+create table asset(assetid integer(6) primary key,assetname varchar(20),assetdes varchar(50),quantity varchar(20),status varchar(20));
 insert into asset values( 100, 'Laptop', 'Lenovo', 20, 'Available');
 insert into asset values( 101, 'Laptop', 'HP', 40, 'Available');
 insert into asset values( 102, 'Laptop', 'Dell', 50, 'Available');
@@ -25,18 +25,18 @@ insert into asset values( 108, 'House', '2BHK', 15, 'Available');
 insert into asset values( 109, 'House', '1BHK', 15, 'Available');
 
 
-create table employee(empno number(6),ename varchar2(10),job varchar2(10),mgrno number(5),hiredate date,deptid number(10));
-insert into employee values( 7839, 'KING', 'PRESIDENT', null, to_date('17-11-1981','dd-mm-yyyy'),10);
-insert into employee values( 7698, 'BLAKE', 'MANAGER', 7839, to_date('1-5-1981','dd-mm-yyyy'), 30);
-insert into employee values( 7782, 'CLARK', 'MANAGER', 7839, to_date('9-6-1981','dd-mm-yyyy'),  10);
-insert into employee values( 7566, 'JONES', 'MANAGER', 7839, to_date('2-4-1981','dd-mm-yyyy'), 20);
-insert into employee values( 7788, 'SCOTT', 'ANALYST', 7566, to_date('13-7-1987','dd-mm-yyyy'), 20);
-insert into employee values( 7902, 'FORD', 'ANALYST', 7566, to_date('3-12-1981','dd-mm-yyyy'), 20);
+create table employee(empno integer(6),ename varchar(10),job varchar(10),mgrno integer(5),hiredate date,deptid integer(10));
+insert into employee values( 7839, 'KING', 'PRESIDENT', null, '1981-11-17',10);
+insert into employee values( 7698, 'BLAKE', 'MANAGER', 7839, '1981-5-1', 30);
+insert into employee values( 7782, 'CLARK', 'MANAGER', 7839,'1981-6-9',  10);
+insert into employee values( 7566, 'JONES', 'MANAGER', 7839, '1981-4-2', 20);
+insert into employee values( 7788, 'SCOTT', 'ANALYST', 7566, '1987-7-13', 20);
+insert into employee values( 7902, 'FORD', 'ANALYST', 7566, '1981-12-3', 20);
 
-create table Department(Dept_ID int, Dept_Name VARCHAR2(50));
+create table Department(Dept_ID integer, Dept_Name VARCHAR(50));
 insert into Department values(10, 'ACCOUNTING');
 insert into Department values(20, 'RESEARCH');
 insert into Department values(30, 'SALES');
 insert into Department values(40, 'OPERATIONS');
 
-create table Asset_Allocation(AllocationId Number, AssetId Number, Empno Number References Employee(Empno) , Allocation_date Date, Release_date Date);
+create table Asset_Allocation(AllocationId INTEGER, AssetId INTEGER, Empno INTEGER References Employee(Empno) , Allocation_date Date, Release_date Date);
